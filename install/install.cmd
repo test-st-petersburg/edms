e42ed30a8e1fc3bfdda195b0c60cd8bda852708c
sergey.s.betke <sergey.s.betke@yandex.ru>
2019-06-20 13:59:24 +0300

    feat(design): Install Precommit1c git hook

--- install/install.cmd
+++ install/install.cmd
@@ -1,9 +1,11 @@
+@echo on
 choco install git.install
 choco install vscode
 choco install nodejs
 choco install onescript -y
 call npm install
 call opm install precommit1c
+call Precommit1c --install
 call opm install gitsync
 call opm install packman
 call opm install deployka
