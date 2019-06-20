2b3d7710c939f1b01dae3752b5f18233d028ca7c
sergey.s.betke <sergey.s.betke@yandex.ru>
2019-06-20 13:54:48 +0300

    chore(design): Install design tools (onescript + packages)

---
+++ install/install.cmd
@@ -0,0 +1,8 @@
+choco install git.install
+choco install vscode
+choco install nodejs
+choco install onescript -y
+call npm install
+call opm install precommit1c
+call opm install gitsync
+call opm install packman
