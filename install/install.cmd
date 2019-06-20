d59fc902bfa7f2db094543c4c16ce6169ebdf1ae
sergey.s.betke <sergey.s.betke@yandex.ru>
2019-06-20 18:38:09 +0300

    chore(design): Reconfigure precommit1c hook

--- install/install.cmd
+++ install/install.cmd
@@ -3,9 +3,14 @@ choco install git.install
 choco install vscode
 choco install nodejs
 choco install onescript -y
+
 call npm install
+
 call opm install precommit1c
 call Precommit1c --install
+@echo #!/bin/sh > .git/hooks/pre-commit
+@echo oscript -encoding=utf-8 .git/hooks/v8files-extractor.os --git-precommit src --remove-orig-bin-files --use-designer >> .git/hooks/pre-commit
+
 call opm install gitsync
 call opm install packman
 call opm install deployka
